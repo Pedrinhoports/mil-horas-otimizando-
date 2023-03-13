6c7e566a94ae006769acde6b6a14f4b5b7c2952a
Pedrinhoports <127267551+Pedrinhoports@users.noreply.github.com>
2023-03-13 09:02:27 -0300

    Add files via upload

---
+++ assets/preload/data/exhale/script.lua
@@ -0,0 +1,21 @@
+function onCreate()
+
+
+	setObjectOrder('dadGroup', 2)
+	setObjectOrder('boyfriendGroup', 1)
+	
+
+end
+
+function onStepHit()
+	
+	
+
+end
+
+function onUpdate(elapsed)
+	
+
+
+
+end
